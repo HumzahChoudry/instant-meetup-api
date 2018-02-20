0b44fae08300d3a29c5be375d05725a380d2e94f
humzahchoudry <Humzah.choudry@gmail.com>
2018-02-20 09:08:50 -0500

    first commit

---
+++ app/controllers/api/v1/meetups_controller.rb
@@ -0,0 +1,23 @@
+class Api::V1::MeetupsController < ApplicationController
+  def index
+    @meetups = Note.all
+    render json: @meetups
+  end
+
+  def update
+    @meetup = Meetup.find(params[:id])
+
+    @meetup.update(meetup_params)
+    if @meetup.save
+      render json: @meetup
+    else
+      render json: {errors: @meetup.errors.full_messages}, status: 422
+    end
+  end
+
+  private
+  def meetup_params
+    params.permit(:title, :content)
+  end
+end
+#HAVE NOT CREATED MODEL YET 
